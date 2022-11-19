b94e655c31facf76d8a373144ff0059346f39ce2
teenyss <mcbrennan@smith.edu>
2022-11-19 16:29:25 -0500

    removed minor unnecessary line

--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -1,7 +1,6 @@
 library(tidyverse)
 library(janitor)
 smith_wbb_data<-read_csv("data-raw/smith_wbb_data.csv") %>%
-  clean_names() %>%
-  select (-english)
+  clean_names()
 
 usethis::use_data(smith_wbb_data, overwrite = TRUE)
