8e79f2f310d61ca3e10bfc42d843bc82755bc4b3
emmagardecki <egardecki@gmail.com>
2022-11-22 13:13:42 -0500

    debugged and checked that package runs w/ no errors

--- R/TS_percentage.R
+++ R/TS_percentage.R
@@ -1,17 +1,20 @@
 #' Calculate TS% or True Shooting Percentage from the Smith WBB dataset.
 #'
+#' @name TS_percentage
 #' @param input_player Player from the Smith WBB roster to calculate stat for.
 #' @param input_season Specifies season within `input_player`s career to calculate stat for.
 #' @return The true shooting percentage of `input_player` in `input_season`.
 #' @export
 #'
 #' @examples
-#' TS_percentage(player = "Bondi, Lauren", season = "2018-19")
-#' input_player <- "Dornbusch, Abby"
-#' input_season <- "2010-11"
-#' TS_percentage(input_player, input_season)
+#' TS_percentage(input_player = "Bondi, Lauren", input_season = "2018-19")
+#' # [1] 56.037
+#' player <- "Dornbusch, Abby"
+#' season <- "2010-11"
+#' TS_percentage(player, season)
+#' # [1] 100
 
-
+utils::globalVariables(c("season", "player", "smith_wbb_data"))
 # base statistic structure
 # TS_percentage <- function(pts, fga, fta) {
 #   100 * (pts/((2 * fga) + (0.88 * fta)))
--- R/perform_players.R
+++ R/perform_players.R
@@ -1,19 +1,21 @@
 #' Compare field goals attempted of players in one season
 #'
 #' @name perform_players
-#' @param player_num1 The number of the player1
-#' @param player_num2 The number of the player2
-#' @param input_season The season of the basketball game
+#' @param player_num1 The number of the first player of interest
+#' @param player_num2 The number of the second player of interest
+#' @param input_season The season of interest from the dataset
 #'
 #' @usage perform_players(player_num1, player_num2, input_season)
 #' @return A bar graph that compares performance of `player1`'s performance with `player2`'s on field goals attempted in one `season`
-#'
-#' @examples
-#' #perform_players(10, 22, "2021-22")
-#'
+#' @export
 #' @import dplyr
 #' @import ggplot2
+#' @import stringr
+#'
+#' @examples
+#' perform_players(10, 22, "2021-22")
 
+utils::globalVariables(c("number", "season", "player", "fga"))
 perform_players <- function(player_num1, player_num2, input_season){
   smith_wbb_data <- as.data.frame(apply(smith_wbb_data,2, str_remove_all, "\r\n"))
 
@@ -28,7 +30,7 @@ perform_players <- function(player_num1, player_num2, input_season){
     geom_bar(stat="identity", position=position_dodge())+
     xlab("Name of player") +
     ylab("Field goals attempted") +
-    ggplot2::ggtitle(paste0(player_1, "v. ", player_2, "of field goals attempted in season ", input_season))
+    ggplot2::ggtitle(paste0(player_1, " v. ", player_2, " field goals attempts, ", input_season, " season."))
 
   return(p_players)
 }
