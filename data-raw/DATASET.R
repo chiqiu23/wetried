867ea237ca3b5d1c0fd954aa449834a6d28b8b3b
teenys <mcbrennan@smith.edu>
2022-12-01 10:31:40 -0500

    made smith_oppwbb_data a viewable file

--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -3,4 +3,8 @@ library(janitor)
 smith_wbb_data<-read_csv("data-raw/smith_wbb_data.csv") %>%
   clean_names()
 
+smith_oppwbb_data <- read_csv("data-raw/smith_oppwbb_data.csv") %>%
+  clean_names
+
 usethis::use_data(smith_wbb_data, overwrite = TRUE)
+usethis::use_data(smith_oppwbb_data, overwrite = TRUE)
