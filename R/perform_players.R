fe4d6ea8671597396c4064a574a7d19eb608285b
emmagardecki <egardecki@gmail.com>
2022-12-06 13:52:11 -0500

    worked on input checks and error messages but still in progress so commented out

--- R/perform_players.R
+++ R/perform_players.R
@@ -15,7 +15,20 @@
 #' @examples
 #' perform_players(10, 22, "2021-22")
 utils::globalVariables(c("number", "season", "player", "fga"))
+
 perform_players <- function(player_num1, player_num2, input_season){
+  ## checks input -- still in progress
+  # stopifnot(
+  #   "`input_season` must match ds. run `unique(smith_wbb_data$season)` for available seasons" =
+  #     isTRUE(match.arg(arg = input_season, choices = unlist(smith_wbb_data$season)) == input_season),
+  #   "`player_num1` must be numeric" = is.numeric(player_num1),
+  #   "`player_num1` must be player number seen in `input_season`" =
+  #     isTRUE(match.arg(arg = as.character(player_num1), choices = as.character(unlist(filter(smith_wbb_data, season == input_season)$number))) == as.character(player_num1)),
+  #   "`player_num2` must be numeric" = is.numeric(player_num2),
+  #   "`player_num2` must be player number seen in `input_season`" =
+  #     isTRUE(match.arg(as.character(player_num2), as.character(unlist(filter(smith_wbb_data, season == input_season)$number))) == as.character(player_num2)),
+  #   "`player_num1` cannot be the same as `player_num2`" = isTRUE(player_num1 != player_num2))
+
   smith_wbb_data <- as.data.frame(apply(smith_wbb_data,2, str_remove_all, "\r\n"))
 
   player_data <- smith_wbb_data |>
